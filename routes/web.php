1a61ba3dda4be59784c30003a8e12c87342c9f65
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-09-05 23:37:49 +0700

    Praktikum 1_Jobsheet 2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/hello', function () {
+    return 'Hello World';
+}); 
+
+Route::get('/world', function () { 
+    return 'World'; 
+    }); 
+
 Route::get('/', function () {
-    return view('welcome');
+    return ('Selamat Datang');
 });
+
+Route::get('/about', function () {
+    return ('(2241760048) Yunika Puteri Dwi Antika');
+});
+
+Route::get('/user/{yunika}', function ($yunika) { 
+    return 'Nama saya '.$yunika; 
+}); 
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) { 
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
+}); 
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' .$id;
+});
+
+// Route::get('/user/{name?}', function ($name=null) {
+//     return 'Nama saya '.$name; 
+// }); 
+
+Route::get('/user/{name?}', function ($name='John') { 
+    return 'Nama saya '.$name; 
+}); 
